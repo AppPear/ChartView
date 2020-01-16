f41cd20c7df9825803f47a82241cf006e9fd6306
Daniel Barclay <barclaysd@me.com>
2020-01-16 18:00:23 +0000

    Ensure animation is never toggled to false onAppear()

--- Sources/SwiftUICharts/LineChart/Line.swift
+++ Sources/SwiftUICharts/LineChart/Line.swift
@@ -15,36 +15,39 @@ struct Line: View {
     @Binding var showIndicator: Bool
     @State private var showFull: Bool = false
     @State var showBackground: Bool = true
-    let padding:CGFloat = 30
+    let padding: CGFloat = 30
     var stepWidth: CGFloat {
         if data.points.count < 2 {
             return 0
         }
-        return frame.size.width / CGFloat(data.points.count-1)
+        return frame.size.width / CGFloat(data.points.count - 1)
     }
+    
     var stepHeight: CGFloat {
-        let points = self.data.onlyPoints()
+        let points = data.onlyPoints()
         if let min = points.min(), let max = points.max(), min != max {
-            if (min <= 0){
-                return (frame.size.height-padding) / CGFloat(points.max()! - points.min()!)
-            }else{
-                return (frame.size.height-padding) / CGFloat(points.max()! + points.min()!)
+            if min <= 0 {
+                return (frame.size.height - padding) / CGFloat(points.max()! - points.min()!)
+            } else {
+                return (frame.size.height - padding) / CGFloat(points.max()! + points.min()!)
             }
         }
         return 0
     }
+    
     var path: Path {
-        let points = self.data.onlyPoints()
+        let points = data.onlyPoints()
         return Path.quadCurvedPathWithPoints(points: points, step: CGPoint(x: stepWidth, y: stepHeight))
     }
+    
     var closedPath: Path {
-        let points = self.data.onlyPoints()
+        let points = data.onlyPoints()
         return Path.quadClosedCurvedPathWithPoints(points: points, step: CGPoint(x: stepWidth, y: stepHeight))
     }
     
     var body: some View {
         ZStack {
-            if(self.showFull && self.showBackground){
+            if self.showFull && self.showBackground {
                 self.closedPath
                     .fill(LinearGradient(gradient: Gradient(colors: [Colors.GradientUpperBlue, .white]), startPoint: .bottom, endPoint: .top))
                     .rotationEffect(.degrees(180), anchor: .center)
@@ -53,15 +56,19 @@ struct Line: View {
                     .animation(.easeIn(duration: 1.6))
             }
             self.path
-                .trim(from: 0, to: self.showFull ? 1:0)
-                .stroke(LinearGradient(gradient: Gradient(colors: [Colors.GradientPurple, Colors.GradientNeonBlue]), startPoint: .leading, endPoint: .trailing) ,style: StrokeStyle(lineWidth: 3))
+                .trim(from: 0, to: self.showFull ? 1 : 0)
+                .stroke(LinearGradient(gradient: Gradient(colors: [Colors.GradientPurple, Colors.GradientNeonBlue]), startPoint: .leading, endPoint: .trailing), style: StrokeStyle(lineWidth: 3))
                 .rotationEffect(.degrees(180), anchor: .center)
                 .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
                 .animation(.easeOut(duration: 1.2))
-                .onAppear(){
+                .onAppear {
                     self.showFull = true
-            }.drawingGroup()
-            if(self.showIndicator) {
+                }
+                .onDisappear {
+                    self.showFull = false
+                }
+                .drawingGroup()
+            if self.showIndicator {
                 IndicatorPoint()
                     .position(self.getClosestPointOnPath(touchLocation: self.touchLocation))
                     .rotationEffect(.degrees(180), anchor: .center)
@@ -71,11 +78,10 @@ struct Line: View {
     }
     
     func getClosestPointOnPath(touchLocation: CGPoint) -> CGPoint {
-        let percentage:CGFloat = min(max(touchLocation.x,0)/self.frame.width,1)
-        let closest = self.path.percentPoint(percentage)
+        let percentage: CGFloat = min(max(touchLocation.x, 0) / frame.width, 1)
+        let closest = path.percentPoint(percentage)
         return closest
     }
-    
 }
 
 extension CGPoint {
@@ -87,37 +93,38 @@ extension CGPoint {
     }
     
     func dist(to: CGPoint) -> CGFloat {
-        return sqrt((pow(self.x - to.x, 2) + pow(self.y - to.y, 2)))
+        return sqrt(pow(x - to.x, 2) + pow(y - to.y, 2))
     }
     
-    static func midPointForPoints(p1:CGPoint, p2:CGPoint) -> CGPoint {
-        return CGPoint(x:(p1.x + p2.x) / 2,y: (p1.y + p2.y) / 2)
+    static func midPointForPoints(p1: CGPoint, p2: CGPoint) -> CGPoint {
+        return CGPoint(x: (p1.x + p2.x) / 2, y: (p1.y + p2.y) / 2)
     }
     
-    static func controlPointForPoints(p1:CGPoint, p2:CGPoint) -> CGPoint {
-        var controlPoint = CGPoint.midPointForPoints(p1:p1, p2:p2)
+    static func controlPointForPoints(p1: CGPoint, p2: CGPoint) -> CGPoint {
+        var controlPoint = CGPoint.midPointForPoints(p1: p1, p2: p2)
         let diffY = abs(p2.y - controlPoint.y)
         
-        if (p1.y < p2.y){
+        if p1.y < p2.y {
             controlPoint.y += diffY
-        } else if (p1.y > p2.y) {
+        } else if p1.y > p2.y {
             controlPoint.y -= diffY
         }
         return controlPoint
     }
 }
+
 extension Path {
-    static func quadCurvedPathWithPoints(points:[Double], step:CGPoint) -> Path {
+    static func quadCurvedPathWithPoints(points: [Double], step: CGPoint) -> Path {
         var path = Path()
-        if (points.count < 2){
+        if points.count < 2 {
             return path
         }
         guard var offset = points.min() else { return path }
         offset -= 3
-        var p1 = CGPoint(x: 0, y: CGFloat(points[0]-offset)*step.y)
+        var p1 = CGPoint(x: 0, y: CGFloat(points[0] - offset) * step.y)
         path.move(to: p1)
         for pointIndex in 1..<points.count {
-            let p2 = CGPoint(x: step.x * CGFloat(pointIndex), y: step.y*CGFloat(points[pointIndex]-offset))
+            let p2 = CGPoint(x: step.x * CGFloat(pointIndex), y: step.y * CGFloat(points[pointIndex] - offset))
             let midPoint = CGPoint.midPointForPoints(p1: p1, p2: p2)
             path.addQuadCurve(to: midPoint, control: CGPoint.controlPointForPoints(p1: midPoint, p2: p1))
             path.addQuadCurve(to: p2, control: CGPoint.controlPointForPoints(p1: midPoint, p2: p2))
@@ -126,18 +133,18 @@ extension Path {
         return path
     }
     
-    static func quadClosedCurvedPathWithPoints(points:[Double], step:CGPoint) -> Path {
+    static func quadClosedCurvedPathWithPoints(points: [Double], step: CGPoint) -> Path {
         var path = Path()
-        if (points.count < 2){
+        if points.count < 2 {
             return path
         }
         guard var offset = points.min() else { return path }
         offset -= 3
         path.move(to: .zero)
-        var p1 = CGPoint(x: 0, y: CGFloat(points[0]-offset)*step.y)
+        var p1 = CGPoint(x: 0, y: CGFloat(points[0] - offset) * step.y)
         path.addLine(to: p1)
         for pointIndex in 1..<points.count {
-            let p2 = CGPoint(x: step.x * CGFloat(pointIndex), y: step.y*CGFloat(points[pointIndex]-offset))
+            let p2 = CGPoint(x: step.x * CGFloat(pointIndex), y: step.y * CGFloat(points[pointIndex] - offset))
             let midPoint = CGPoint.midPointForPoints(p1: p1, p2: p2)
             path.addQuadCurve(to: midPoint, control: CGPoint.controlPointForPoints(p1: midPoint, p2: p1))
             path.addQuadCurve(to: p2, control: CGPoint.controlPointForPoints(p1: midPoint, p2: p2))
@@ -158,17 +165,16 @@ extension Path {
         
         let f = pct > comp ? comp : pct
         let t = pct > comp ? 1 : pct + diff
-        let tp = self.trimmedPath(from: f, to: t)
+        let tp = trimmedPath(from: f, to: t)
         
         return CGPoint(x: tp.boundingRect.midX, y: tp.boundingRect.midY)
     }
-    
 }
 
 struct Line_Previews: PreviewProvider {
     static var previews: some View {
-        GeometryReader{ geometry in
-            Line(data: ChartData(points: [12,-230,10,54]), frame: .constant(geometry.frame(in: .local)), touchLocation: .constant(CGPoint(x: 100, y: 12)), showIndicator: .constant(true))
+        GeometryReader { geometry in
+            Line(data: ChartData(points: [12, -230, 10, 54]), frame: .constant(geometry.frame(in: .local)), touchLocation: .constant(CGPoint(x: 100, y: 12)), showIndicator: .constant(true))
         }.frame(width: 320, height: 160)
     }
 }
