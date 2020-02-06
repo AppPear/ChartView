95048ab946f938fabbe98016a035966e75aefc54
Yon Montoto <yon.montoto@gmail.com>
2020-02-06 16:04:53 -0500

    Reverts dependency

--- Package.swift
+++ Package.swift
@@ -17,7 +17,6 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Yonodactyl/ChartView.git", from: "1.4.7")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
