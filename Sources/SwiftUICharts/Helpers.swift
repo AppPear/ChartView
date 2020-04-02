ba548150e6a3f4c4db55e0eb1d6731258b7ecbe2
aaronbelsham <aaron@smod.com.au>
2020-04-02 21:33:56 +1000

    Resolves blank chart when choosing .large option.

--- Sources/SwiftUICharts/Helpers.swift
+++ Sources/SwiftUICharts/Helpers.swift
@@ -134,12 +134,12 @@ public struct ChartForm {
     #if os(watchOS)
     public static let small = CGSize(width:120, height:90)
     public static let medium = CGSize(width:120, height:160)
-    public static let large = CGSize(width:180, height:90)
+    public static let large = CGSize(width:180, height:160)
     public static let detail = CGSize(width:180, height:160)
     #else
     public static let small = CGSize(width:180, height:120)
     public static let medium = CGSize(width:180, height:240)
-    public static let large = CGSize(width:360, height:120)
+    public static let large = CGSize(width:360, height:240)
     public static let detail = CGSize(width:180, height:120)
     #endif
     
