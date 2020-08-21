9c4125a6befcd1ce7ea88ca5a96c21bc8e721389
Dan Wood <danwood@users.noreply.github.com>
2020-08-21 15:56:34 -0700

    getting close - some hard-wired values to fix

--- Sources/SwiftUICharts/Charts/RingsChart/Ring.swift
+++ Sources/SwiftUICharts/Charts/RingsChart/Ring.swift
@@ -3,32 +3,184 @@
 //  ChartViewV2Demo
 //
 //  Created by Dan Wood on 8/20/20.
-//
+//	Based on article and playground code by Frank Jia
+//	https://medium.com/@frankjia/creating-activity-rings-in-swiftui-11ef7d336676
 
 import SwiftUI
 
-struct Ring: View {
 
-	@State private var stroke : CGFloat = 16.0
-	@State private var fraction : CGFloat = 0.75
-    var body: some View {
-        Circle()
-			.fill(Color.white)
-			.overlay(
-				Circle()
-					.trim(from:0, to:fraction)
-					.stroke(style: StrokeStyle(lineWidth:stroke, lineCap: .round, lineJoin: .round))
-					.fill(AngularGradient(gradient: .init(colors:[.red, .orange]), center: .center, startAngle:.zero, endAngle: .init(degrees:360)))
-					.rotationEffect(Angle(degrees: 270.0))
-			)
-			.padding(stroke/2.0)
-			.animation(.spring(response:1.0, dampingFraction:1.0, blendDuration:1.0))
-    }
+extension Double {
+	func toRadians() -> Double {
+		return self * Double.pi / 180
+	}
+	func toCGFloat() -> CGFloat {
+		return CGFloat(self)
+	}
 }
 
-struct RingChart_Previews: PreviewProvider {
-    static var previews: some View {
-        Ring()
-			.padding()
-    }
+struct RingShape: Shape {
+	// Helper function to convert percent values to angles in degrees
+	static func percentToAngle(percent: Double, startAngle: Double) -> Double {
+		(percent / 100 * 360) + startAngle
+	}
+	private var percent: Double
+	private var startAngle: Double
+	private let drawnClockwise: Bool
+
+	// This allows animations to run smoothly for percent values
+	var animatableData: Double {
+		get {
+			return percent
+		}
+		set {
+			percent = newValue
+		}
+	}
+
+	init(percent: Double = 100, startAngle: Double = -90, drawnClockwise: Bool = false) {
+		self.percent = percent
+		self.startAngle = startAngle
+		self.drawnClockwise = drawnClockwise
+	}
+
+	// This draws a simple arc from the start angle to the end angle
+	func path(in rect: CGRect) -> Path {
+		let width = rect.width
+		let height = rect.height
+		let radius = min(width, height) / 2
+		let center = CGPoint(x: width / 2, y: height / 2)
+		let endAngle = Angle(degrees: RingShape.percentToAngle(percent: self.percent, startAngle: self.startAngle))
+		return Path { path in
+			path.addArc(center: center, radius: radius, startAngle: Angle(degrees: startAngle), endAngle: endAngle, clockwise: drawnClockwise)
+		}
+	}
+}
+
+struct Ring: View {
+
+	private static let ShadowColor: Color = Color.black.opacity(0.2)
+	private static let ShadowRadius: CGFloat = 5
+	private static let ShadowOffsetMultiplier: CGFloat = ShadowRadius + 2
+
+	private let ringWidth: CGFloat
+	private let percent: Double
+	private let backgroundColor: ColorGradient
+	private let foregroundColor: ColorGradient
+	private let startAngle: Double = -90
+	private var gradientStartAngle: Double {
+		self.percent >= 100 ? relativePercentageAngle - 360 : startAngle
+	}
+	private var absolutePercentageAngle: Double {
+		RingShape.percentToAngle(percent: self.percent, startAngle: 0)
+	}
+	private var relativePercentageAngle: Double {
+		// Take into account the startAngle
+		absolutePercentageAngle + startAngle
+	}
+	private var lastGradientColor: Color {
+		self.foregroundColor.endColor
+	}
+	private var backgroundGradient: AngularGradient {
+		AngularGradient(
+			gradient: self.backgroundColor.gradient,
+			center: .center,
+			startAngle: Angle(degrees: 0),
+			endAngle: Angle(degrees: 360)
+		)
+	}
+
+	private var ringGradient: AngularGradient {
+		AngularGradient(
+			gradient: self.foregroundColor.gradient,
+			center: .center,
+			startAngle: Angle(degrees: self.gradientStartAngle),
+			endAngle: Angle(degrees: relativePercentageAngle)
+		)
+	}
+
+	init(ringWidth: CGFloat, percent: Double, backgroundColor: ColorGradient, foregroundColor: ColorGradient) {
+		self.ringWidth = ringWidth
+		self.percent = percent
+		self.backgroundColor = backgroundColor
+		self.foregroundColor = foregroundColor
+	}
+
+	var body: some View {
+		GeometryReader { geometry in
+			ZStack {
+				// Background for the ring
+				RingShape()
+					.stroke(style: StrokeStyle(lineWidth: self.ringWidth))
+					.fill(self.backgroundGradient)
+				// Foreground
+				RingShape(percent: self.percent, startAngle: self.startAngle)
+					.stroke(style: StrokeStyle(lineWidth: self.ringWidth, lineCap: .round))
+					.fill(self.ringGradient)
+				// End of ring with drop shadow
+				if self.getShowShadow(frame: geometry.size) {
+					Circle()
+						.fill(self.lastGradientColor)
+						.frame(width: self.ringWidth, height: self.ringWidth, alignment: .center)
+						.offset(x: self.getEndCircleLocation(frame: geometry.size).0,
+								y: self.getEndCircleLocation(frame: geometry.size).1)
+						.shadow(color: Ring.ShadowColor,
+								radius: Ring.ShadowRadius,
+								x: self.getEndCircleShadowOffset().0,
+								y: self.getEndCircleShadowOffset().1)
+				}
+			}
+		}
+		// Padding to ensure that the entire ring fits within the view size allocated
+		.padding(self.ringWidth / 2)
+	}
+
+	private func getEndCircleLocation(frame: CGSize) -> (CGFloat, CGFloat) {
+		// Get angle of the end circle with respect to the start angle
+		let angleOfEndInRadians: Double = relativePercentageAngle.toRadians()
+		let offsetRadius = min(frame.width, frame.height) / 2
+		return (offsetRadius * cos(angleOfEndInRadians).toCGFloat(), offsetRadius * sin(angleOfEndInRadians).toCGFloat())
+	}
+
+	private func getEndCircleShadowOffset() -> (CGFloat, CGFloat) {
+		let angleForOffset = absolutePercentageAngle + (self.startAngle + 90)
+		let angleForOffsetInRadians = angleForOffset.toRadians()
+		let relativeXOffset = cos(angleForOffsetInRadians)
+		let relativeYOffset = sin(angleForOffsetInRadians)
+		let xOffset = relativeXOffset.toCGFloat() * Ring.ShadowOffsetMultiplier
+		let yOffset = relativeYOffset.toCGFloat() * Ring.ShadowOffsetMultiplier
+		return (xOffset, yOffset)
+	}
+
+	private func getShowShadow(frame: CGSize) -> Bool {
+		if self.percent >= 100 {
+			return true
+		}
+		let circleRadius = min(frame.width, frame.height) / 2
+		let remainingAngleInRadians = (360 - absolutePercentageAngle).toRadians().toCGFloat()
+
+		return circleRadius * remainingAngleInRadians <= self.ringWidth
+	}
+}
+
+struct Ring_Previews: PreviewProvider {
+	static var previews: some View {
+		VStack {
+			Ring(
+				ringWidth: 50, percent: 5 ,
+				backgroundColor: ColorGradient(Color.green.opacity(0.2)),
+				foregroundColor: ColorGradient(.green, .blue)
+			)
+			.frame(width: 200, height: 200)
+
+			Ring(
+				ringWidth: 20, percent: 110 ,
+				backgroundColor: ColorGradient(Color.black.opacity(0.2)),
+				foregroundColor: ColorGradient(.red, .blue)
+			)
+			.frame(width: 200, height: 200)
+
+
+
+		}
+	}
 }
--- Sources/SwiftUICharts/Charts/RingsChart/RingsChartRow.swift
+++ Sources/SwiftUICharts/Charts/RingsChart/RingsChartRow.swift
@@ -29,13 +29,14 @@ public struct RingsChartRow: View {
 		GeometryReader { geometry in
 			ZStack {
 				ForEach(0..<self.chartData.data.count, id: \.self) { index in
-					Ring()
+					Ring(ringWidth: 20, percent: self.chartData.data[index], backgroundColor: self.style.backgroundColor, foregroundColor:self.style.foregroundColor.rotate(for: index))
 //					value: self.normalizedValue(index: index),
 //								 index: index,
 //								 width: Float(geometry.frame(in: .local).width - Constant.spacing),
 //								 numberOfDataPoints: self.chartData.data.count,
 //								 gradientColor: self.style.foregroundColor.rotate(for: index),
 //								 touchLocation: self.touchLocation)
+						.padding(20.0 * CGFloat(index))
 						.scaleEffect(self.getScaleSize(touchLocation: self.touchLocation, index: index), anchor: .center)
 						.animation(Animation.easeIn(duration: 0.2))
 				}
@@ -64,3 +65,19 @@ public struct RingsChartRow: View {
 	}
 }
 
+
+struct RingsChartRow_Previews: PreviewProvider {
+	static var previews: some View {
+
+		let multiStyle = ChartStyle(backgroundColor: .gray,
+										   foregroundColor:
+											[ColorGradient(.purple, .blue),
+											 ColorGradient(.orange, .red),
+											 ColorGradient(.green, .yellow),
+											 ColorGradient(.red, .purple),
+											 ColorGradient(.yellow, .orange),
+											])
+
+		return RingsChartRow(chartData: ChartData([25,50,75,100,125,150]), style: multiStyle)
+	}
+}
