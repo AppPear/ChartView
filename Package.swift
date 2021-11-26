9b21759c9abde02d2f0ee7e284c2195062adc0bc
Niklas Saers <niklas@saers.com>
2021-11-26 10:19:42 +0100

    Just targetting modern builds

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftUICharts",
     platforms: [
-        .iOS(.v13), .watchOS(.v6), .macOS(.v11)
+        .iOS(.v14), .watchOS(.v7), .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
