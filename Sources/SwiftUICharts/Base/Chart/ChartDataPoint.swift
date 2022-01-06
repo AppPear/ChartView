389ab20af7f14c354c7bc94fe691eed31d18615b
Carlton Jester <jcarltonj@gmail.com>
2022-01-06 00:07:10 -0600

    Update documentation

--- Sources/SwiftUICharts/Base/Chart/ChartDataPoint.swift
+++ Sources/SwiftUICharts/Base/Chart/ChartDataPoint.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 public protocol ChartDataPoint: Equatable {
-   
+    
+    /// The value shown for the label of the data in the basic form
     var chartValue: String { get }
 }
--- Sources/SwiftUICharts/Base/Chart/ChartValue.swift
+++ Sources/SwiftUICharts/Base/Chart/ChartValue.swift
@@ -6,8 +6,11 @@ public class SimpleChartValue: ChartValue {
     @Published public var interactionInProgress: Bool = false
 }
 
+/// The object that is used to report the currently selected ChartDataPoint and if the user is interacting with the chart
 public protocol ChartValue: ObservableObject {
     associatedtype Root: ChartDataPoint
+    /// The current or last known value the user interacted with.  This value should be @Published for proper reporting
     var currentValue: Root? { get set }
+    /// If the user is currently interacting, this value is true.  This value should be @Published for proper reporting
     var interactionInProgress: Bool { get set }
 }
