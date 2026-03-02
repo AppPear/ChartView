2a1572430eacc31311a9b23d7dfa1e9cf9e49234
Andras Samu <andras@teleprompter.com>
2026-03-02 09:25:57 +0100

    Add Apple privacy manifest for SwiftUICharts SDK

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -23,7 +23,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftUICharts",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .process("PrivacyInfo.xcprivacy")
+            ]),
         .testTarget(
             name: "SwiftUIChartsTests",
             dependencies: ["SwiftUICharts"])
